4cf45076d2e2ddb50dda2456c230fc768a98ada9
a.vladimirov <archibaldvladimirov@gmail.com>
2023-06-17 19:59:06 +0300

    finals: A commentary done

--- sprint7/finals/A.cpp
+++ sprint7/finals/A.cpp
@@ -1,8 +1,76 @@
 // accepted parcel ID: -
 //
-// Problem description  
+// Problem
+//  Given two strings s and t compute the Levenstien distance d(s,t).
 //
-// Solution
+// Definitions and assumptions                                      // notation
+//  1. atomic transform of a string s is one of the following       // a
+//          - delete an arbitrary letter                            // del
+//          - insert an arbitrary letter                            // ins
+//          - replace an arbitrary letter with another              // rep
+//  2. we will condsider a class of string transformations that are the 
+//     composition of atomic
+//  3. for a transform f = a_1 * a_2 * ... * a_n (a-s are atomic) denote the
+//     transform complexity |f| = n                                 // |f|
+//  4. d(s,t) = min {|f|, f : f(s) = f(t)}                          // d(s,t)
+//     note: it may be trivially proven the d(s,t) is indeed a distance
+//  5. we denote an empty string as emp. Note that for any string s // emp
+//     d(s,emp) = |s|
+//
+// Statement
+//              d(s,t) = min{   d(s1..s{n-1}, t) + 1,
+//                              d(s,t_1..t{m-1}) + 1,                        (1)
+//                              d(s_1..s_{n-1}, t_1..t_{m-1}) + I(s_n != t_m)
+//                          },
+//              
+// Proof
+//  Consider two non empty strings s = s_1s_2...s_n and  t = t_1t_2...t_m and 
+// an optimal transform f : f(s) = t, |f| = d(s,t).
+//  
+//  Consider the last element of s -- s_n. The transform f may have done with it
+// one of the following:
+//      1. delete s_n. Then f(s) = g(s_1..s_{n-1}) + del(s_n) = t. Where 
+//         g(s_1..s{n-1}) = t, and g is optimal, otherwise f would not be. And 
+//         we get the following 
+//                  d(s,t) = |f| = |g| + |del| = d(s1..s{n-1}, t) + 1
+//      2. replace s_n with another letter
+//         2.a s_n cannot be replaced with none of the t_1,..t_m, otherwise f
+//             is not optimal
+//         2.b if s_n is replaced with t_i, i < m (i.e. not the last t letter),
+//             then f(s) = g(s) + ins(t_m). Where g(s) = t_1...t_{m-1}, an 
+//             optimal transform. Same as in (1) we get
+//                  d(s,t) = d(s,t_1..t{m-1}) + 1
+//         2.c if s_n is replaced with t_m (the last t letter), then 
+//             f(s) = g(s1..s{n-1}) + rep(s_n), where g(s1..s{n-1}) = t1..t{m-1}
+//             and rep(s_n) = t_m. Now we get
+//                  d(s,t) = d(s_1..s_{n-1}, t_1..t_{m-1}) + I(s_n != t_m),
+//             where "I" stays for indicator.
+//
+//  As these are all of the options, we end up with the formula (1). The 
+// optimal f may be recovered as a composition of argmin pair (g,a) of an 
+// optimal g and an atomic tranform a.
+//
+// Algorithm
+//  The statement implies the algorithm directly. Given the formula (1) and 
+// the base case (def. & ass. 5). We may compute d(s,t) for an arbitrary pair 
+// of strings s and t by sequentially computing distance between all prefixes.
+//
+//  Indeed, we compute a matrix dist of size (n+1)*(m+1) with elements 
+// dist[i][j] = d(s_1..s_i, t_1..t_j), where i or j = 0 stays for null prefix.
+//  With 0th row and column being initialized according to the (def. & ass. 5),
+// the matrix may be computed row by row with direct use of formula (1), i.e.
+//              dist[i][j] = min(   dist[i-1][ j ] + 1,
+//                                  dist[ i ][j-1] + 1,
+//                                  dist[i-1][j-1] + I(s[i] != t[j])
+//                      )
+//  Clearly, the answer d(s,t) would lie in dist[n][m].
+//
+// Complexity
+//  Space O(nm)*
+//  Time O(nm)
+//
+// * as for the computation of each row only the previous is needed this may be
+// easily reduced to O(n) or O(m).
 //
 
 #include <iostream>
